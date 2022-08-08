e7020eba72ab2bf5daf1ce89d0ed5fe295b61729
Codeyg12 <codey.gallup@gmail.com>
2022-08-08 17:44:13 -0600

    added error page as well as more server code

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-const express = require('express')
-const path = require('path')
-const util = require('util')
+const express = require("express");
+const path = require("path");
+const fs = require('fs')
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-const app = express()
+app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3001
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-app.use(express.static('public'))
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
+app
+  .route("/api/notes")
+  .get((req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+  })
+  .post((req, res) => {
+    let newNote = req.body;
+    let notesList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    notesList.push(newNote);
 
-app.listen(PORT)
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesList));
+    res.json(notesList);
+  });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/error.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Now running on ${PORT}`);
+});
 
 //npm run devStart
